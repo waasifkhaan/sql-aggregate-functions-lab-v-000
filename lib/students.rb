3b735a1b3e9688a4a0a562c48cdf57fbe3f7952a
Wasif Khan <waasif.khaan@gmail.com>
2018-07-30 21:56:03 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-  select 
+  select gpa from students where 
 end
 
 def lowest_student_gpa
